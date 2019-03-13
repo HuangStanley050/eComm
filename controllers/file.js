9ba06511fba44801aca9365f21c5bd9176ab6254
Stanley Huang <infamous_godhand@yahoo.com>
2019-03-13 11:56:20 +0000

    some changes to file controller

--- controllers/file.js
+++ controllers/file.js
@@ -1,7 +1,9 @@
 const Product = require("../models/product");
 exports.fetchProducts = (req, res, next) => {
   const pageNumber = req.query.page;
+
   Product.paginate({}, { page: pageNumber, limit: 3 })
+    //result.totalDocs
     .then(result => res.json(result))
     .catch(err => next(err));
 };
