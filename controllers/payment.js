81f3f80fb82730e5beffc16ee8d353304d3bc857
Stanley Huang <infamous_godhand@yahoo.com>
2019-03-21 07:48:00 +0000

    can charge the payment now

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,3 +1,20 @@
-exports.makePayment = (req, res, next) => {
-  res.send("payment route++");
+const secretKey = process.env.STRIPE_KEY;
+const stripe = require("stripe")(secretKey);
+
+exports.makePayment = async (req, res, next) => {
+  const token = req.body.token;
+  const amount = req.body.amount;
+  //console.log(amount);
+  try {
+    let result = await stripe.charges.create({
+      amount: amount,
+      source: token.id,
+      currency: "usd"
+    });
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+  //res.json(token);
 };
