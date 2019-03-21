40ed7cd476d13e20d9606c5d33ffc692a829abfc
Stanley Huang <infamous_godhand@yahoo.com>
2019-03-21 23:20:39 +0000

    can add user to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,36 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwt_secret = process.env.JWTSECRET;
+
 exports.regiser = (req, res, next) => {
   //console.log(req.body);
-  res.json(req.body);
+  const { name, email, password } = req.body;
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        const error = new Error();
+        error.message = "user already signed up";
+        error.status = 404;
+        throw error;
+      }
+
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedpassword => {
+      const newUser = User({
+        name,
+        email,
+        password: hashedpassword
+      });
+      return newUser.save();
+    })
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 exports.login = (req, res, next) => {};
