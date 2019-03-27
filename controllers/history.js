5b987a1f816b8b8470204a81eb5d8f71c17e94f8
Stanley Huang <infamous_godhand@yahoo.com>
2019-03-27 09:11:41 +0000

    able to return the history of purchase base on id, some drama with mongoId, needs to convert to string to compare

--- controllers/history.js
+++ controllers/history.js
@@ -1,3 +1,21 @@
-exports.getHistory = (req, res, next) => {
-  res.send("history route");
+const Order = require("../models/order");
+//const mongoose = require("mongoose");
+
+exports.getHistory = async (req, res, next) => {
+  const purchaserId = req.user.id;
+  let all_orders = [];
+  let history = [];
+  try {
+    all_orders = await Order.find();
+    all_orders.forEach(order => {
+      if (order.purchaserId.toString() === purchaserId) {
+        history.push(order);
+      }
+    });
+    console.log(history);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+  res.json(history);
 };
